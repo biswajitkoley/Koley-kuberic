c2c8db701143b2b4ea4c19db939672e476bb8862
biswajitkoley <68938835+biswajitkoley@users.noreply.github.com>
2020-11-22 21:08:38 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -8,14 +8,23 @@ import sys
 TRAIN_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_train.csv"
 TEST_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_test.csv"
 
-
+response = requests.get(TRAIN_DATA_URL)
+with open('train_area.csv', 'w') as file:
+    file.write(response.content.decode('utf-8')[5:2753])
+with open('train_price.csv', 'w') as file:
+    file.write(response.content.decode('utf-8')[2759:])
+y = numpy.array(pandas.read_csv('train_price.csv').columns)
+x = numpy.array(pandas.read_csv('train_area.csv').columns)
+x = numpy.array(list(map(float, x)))
+y = numpy.array(list(map(float, y)))
+slope, intercept, _, _, _ = stats.linregress(x, y)
 def predict_price(area) -> float:
     """
     This method must accept as input an array `area` (represents a list of areas sizes in sq feet) and must return the respective predicted prices (price per sq foot) using the linear regression model that you build.
 
     You can run this program from the command line using `python3 regression.py`.
     """
-    response = requests.get(TRAIN_DATA_URL)
+    return intercept + slope * area
     # YOUR IMPLEMENTATION HERE
     ...
 
